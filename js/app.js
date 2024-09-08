e1e511d04b937e9f12d3a8771ec03d5c99c16455
john <eric.braden@erics-solutions-ohio.com>
2024-09-08 19:56:07 -0400

    feat(HTML to JSON) saving more progress

--- js/app.js
+++ js/app.js
@@ -39,44 +39,95 @@ function getThisColsTasks(){
 
 function makeJsonFromHTML(){
     var allProjects = document.getElementsByClassName('oneProject');
-    var json = {};
+    var outputJsonProjects = [];
 
+    for (var i=0; i<allProjects.length; i++){
+        var thisProjectsJson = extractProjectData(allProjects[i]);
+        console.log(thisProjectsJson);
+    }
     Object.keys(allProjects)
         .filter((k, i) => i >= 0 && i < 300)
         .forEach((k)=>{
-            var thisData = extractProjectData(allProjects[k]);
-            console.log(thisData);
+            var thisProjectsJson = extractProjectData(allProjects[k]);
+            // console.log(thisData);
         });
+    var thisProjectData = extractProjectData(outputJsonProjects);
+    outputJsonProjects.push(thisProjectData);
 
     var keys = Object.keys(allProjects);
-    console.log(keys);
+    // console.log(keys);
 
 
     // Function to extract project data
     function extractProjectData(projectElement) {
-        var projectName = getProjectName();
-        var projectThumbUrl = getProjectThumbUrl();
-        var projectTitle = getProjectTitle();
-        var columns = getColumns();
-        var thisColsHeading = getThisColsHeading();
-        var thisColsID = getThisColsID();
-        var thisColsTasks = getThisColsTasks();
+        var thisProjectsJson;
+        var projectName = getProjectName(projectElement);
+        var projectThumbUrl = getProjectThumbUrl(projectElement);
+        var projectTitle = getProjectTitle(projectElement);
+        var columns;
+        var thisColsHeading;
+        var thisColsID;
+        var thisColsTasks;
+
+
+        function getColumns(projectElement){
+            var thisColumn;
+            return thisColumn;
+        };
+        function getThisColsHeading(col){
+            var thisColsHeading;
+            return thisColsHeading;
+        };
+        function getThisColsID(col){
+            var thisColsID;
+            return thisColsID;
+        };
+        function getThisColsTasks(col){
+            var thisColsTasks;
+            return thisColsTasks;
+        };
+
+
+
+        columns = getColumns(projectElement);
+
+        columns.ForEach((col) => {
+            thisColsHeading = getThisColsHeading(col);
+            thisColsID = getThisColsID(col);
+            thisColsTasks = getThisColsTasks(col);
+            
+        });
+        thisColsHeading = getThisColsHeading(projectElement);
+        thisColsID = getThisColsID(projectElement);
+        thisColsTasks = getThisColsTasks(projectElement);
 
         // THIS NEEDS TO BE NESTED OR RECURSIVELY CALLED OR SOMETHING ELSE
 
-        return {
+        thisProjectsJson = {
             "name": projectName,
             "thumbnailUrl": projectThumbUrl, 
             "projectTitle": projectTitle,
             "columns": [
-                "elementID": "col2Ready",  
-                "heading": "Ready",
-                "tasks": [
-                    {"text": "Toptal"}, 
-                    {"text": "Fiverr"}, 
-                    {"text": "Upwork"}, 
-                    {"text": "99 Designs"}
-                ]
+                {
+                    "elementID": "col2Ready",  
+                    "heading": "Ready",
+                    "tasks": [
+                        {"text": "Toptal"}, 
+                        {"text": "Fiverr"}, 
+                        {"text": "Upwork"}, 
+                        {"text": "99 Designs"}
+                    ]
+                },
+                {
+                    "elementID": "col2Ready",  
+                    "heading": "Ready",
+                    "tasks": [
+                        {"text": "Toptal"}, 
+                        {"text": "Fiverr"}, 
+                        {"text": "Upwork"}, 
+                        {"text": "99 Designs"}
+                    ]
+                }
             ]
         };
     }
@@ -85,9 +136,21 @@ function makeJsonFromHTML(){
     // const json = JSON.stringify(projects, null, 2);
 
     // Output JSON (e.g., log to console or save to a file)
-    console.log('\n\n JSON: ');
-    console.log(json);
+    // console.log('\n\n JSON: ');
+    // console.log(json);
 }
+
+
+
+
+function makeNewWidgets(allProjects){
+    allProjects.forEach((thisProjectJson) => {
+        var thisProjectName = thisProjectJson.name;
+        var thisTaskManWidget = new taskManWidget(thisProjectJson, thisProjectName, projectsWrapperID);
+    });
+}
+
+
 
 function makeAllProjects(){
     fetch(jsonUrl)
@@ -99,10 +162,66 @@ function makeAllProjects(){
         })
         .then(data => {
             var allProjects = data.projects;
+
+
+
+
+
+
+var thisPro = {
+                "name": "pun chicken", 
+                "thumbnailUrl": "./images/freelancingCode.jpg",
+                "projectTitle": "Freelancing",
+                "columns": [
+                    {
+                        "elementID": "col1Backlog",  
+                        "heading": "Backlog",
+                        "tasks": [
+                        ]
+                    }, 
+                    {
+                        "elementID": "col2Ready",  
+                        "heading": "Ready",
+                        "tasks": [
+                            {"text": "Toptal"}, 
+                            {"text": "Fiverr"}, 
+                            {"text": "Upwork"}, 
+                            {"text": "99 Designs"}
+                        ]
+                    }, 
+                    {
+                        "elementID": "col3InProgress",  
+                        "heading": "In Progress",
+                        "tasks": [
+                        ]
+                    }, 
+                    {
+                        "elementID": "col4Review",  
+                        "heading": "Review",
+                        "tasks": [
+                        ]
+                    }, 
+                    {
+                        "elementID": "col5Done",  
+                        "heading": "Done",
+                        "tasks": [
+                        ]
+                    }
+                ]
+            };
+
+
+
+
+
+            // data.projects.push(thisPro);
+            // console.log(data);
+            makeNewWidgets(allProjects);
             allProjects.forEach((thisProjectJson) => {
                 var thisProjectName = thisProjectJson.name;
                 var thisTaskManWidget = new taskManWidget(thisProjectJson, thisProjectName, projectsWrapperID);
             })
+
             makeJsonFromHTML();
         })
         .catch(error => {
